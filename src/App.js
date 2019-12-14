399db10a39b5cc51f1f8f8fe30f555b462e11232
Aswanth vp <aswanthv2@gmail.com>
2019-12-14 14:34:28 +0530

    latest change

--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src="https://img.icons8.com/nolan/64/000000/remittance-slip.png" className="App-logo" alt="logo" />
+          <h1 className="app-name">EasyBill</h1>
+          
+        <Buttonshome/>
+        <div id="mainContent">
+          
+        </div>
+        </header>
+      </div>
+    );
+  }
 }
+
+
+
+class Buttonshome extends React.Component{
+  constructor(props){
+    super(props);
+    this.state={names: ["Orders","Home"]};
+  }
+  render(){
+    return(
+      <div className="main">
+        {
+          this.state.names.map((value, index) => {
+            return <ButtonsContent key={value.toString()} name={value}></ButtonsContent>
+          })
+        }
+      </div>
+    );
+  }
+}
+
+class ButtonsContent extends React.Component{
+  render(){
+    return(
+      <div className="tablink">
+        <p>{this.props.name}</p>
+      </div>
+    );
+  }
+}
+
+
+
+
 
 export default App;
