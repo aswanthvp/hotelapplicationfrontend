0d70aa18ebf6e872a17bfb7180d41d80794954fb
Aswanth vp <aswanthv2@gmail.com>
2019-12-28 00:53:51 +0530

    orders folder added in components

---
+++ src/component/orders/Orders.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+// import ReactTable from "react-table"
+// import 'react-table/react-table.css'
+import Title from "../Title"
+export default class Orders extends Component {
+    render() {
+        return (
+            <div>
+                <Title name="Current" title="Orders" />
+                <div className="container card test py-5">
+                    <div className="row">
+                        <div className="col-12">
+
+                        </div>
+                        <div className="col-12">
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
