01582597ac43425384fce04ebafc06366cef6484
Aswanth vp <aswanthv2@gmail.com>
2019-12-28 16:15:27 +0530

    removed react-alert import

--- src/index.js
+++ src/index.js
@@ -4,24 +4,12 @@ import {BrowserRouter as Router} from 'react-router-dom'
 import App from './App.js';
 import {FoodProvider} from './context'
 import * as serviceWorker from './serviceWorker';
-import { transitions, positions, Provider as AlertProvider } from 'react-alert'
-import AlertTemplate from 'react-alert-template-basic'
 
-const options = {
-    // you can also just use 'bottom center'
-    position: positions.BOTTOM_CENTER,
-    timeout: 5000,
-    offset: '30px',
-    // you can also just use 'scale'
-    transition: transitions.SCALE
-}
 
 ReactDOM.render(
     <FoodProvider>
         <Router>
-            <AlertProvider template={AlertTemplate} {...options}>
-                <App />
-            </AlertProvider>
+            <App />
         </Router>
     </FoodProvider>
 ,
